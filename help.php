a93c4bab62ac9533cd6fa74d590aad991cd9a542
Marshall Nguyen <64998098+marshall-nguyen@users.noreply.github.com>
2020-06-15 19:12:44 -0500

    Update help.php
    Testing marshall edit

--- help.php
+++ help.php
@@ -10,7 +10,7 @@
  <div class="right-content">
     <div class="container">
 
-      <h3 style = "color: #01B0F1;">HELP (TO BE DONE LATER)</h3>
+      <h3 style = "color: #01B0F1;">HELP will be displayed here!</h3>
 
     </div>
 </div>
